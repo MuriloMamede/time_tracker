5a7960785bfd11a039b3ca5d80ddf0f16d0cf578
MuriloMamede <muriloomamede@gmail.com>
2020-03-13 21:30:30 -0400

    added dependency injection

---
+++ lib/services/auth.dart
@@ -0,0 +1,34 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
+
+class User{
+  User({@required this.uid});
+  final String uid;
+}
+
+
+class Auth{
+  
+  final _firebaseAuth = FirebaseAuth.instance;
+  User _userFromFirebase(FirebaseUser user){
+    if(user == null){
+      return null;
+    }
+    return User(uid: user.uid);
+  }
+
+  Future<User> currentUser() async{
+    final user = await _firebaseAuth.currentUser();
+    return _userFromFirebase(user);
+  }
+
+  Future<User> signInAnonymously() async{
+    final authResult = await _firebaseAuth.signInAnonymously();
+    return _userFromFirebase(authResult.user);
+  }
+
+  Future<void> signOut() async{
+    return await _firebaseAuth.signOut();
+  }
+
+}
