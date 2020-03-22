692c6c987f469ca36c4d5fd611bc529510913dce
MuriloMamede <muriloomamede@gmail.com>
2020-03-21 23:52:31 -0400

    added streams

--- lib/app/landing_page.dart
+++ lib/app/landing_page.dart
@@ -1,3 +1,5 @@
+
+
 import 'package:flutter/material.dart';
 import 'package:time_tracker/app/homePage.dart';
 import 'package:time_tracker/app/sign_in/sign_in_page.dart';
@@ -16,6 +18,9 @@ class _Landing_pageState extends State<Landing_page> {
   void initState(){
     super.initState();
     _checkCurrentUser();
+    widget.auth.onAuthStateChanged.listen((user){
+      print('user: ${user?.uid} ');
+    });
   }
 
   Future<void> _checkCurrentUser() async{
@@ -23,26 +28,41 @@ class _Landing_pageState extends State<Landing_page> {
     _updateUser(user);
   }
 
-
-
   User _user;
   void _updateUser(User user){
     setState(() {
       _user = user;
-    });
-    
+    });    
   }
   @override
   Widget build(BuildContext context) {
-    if(_user == null){
-      return SignInPage(
-        onSignIn: _updateUser,
-        auth: widget.auth,
-      );
-    }
-    return HomePage(onSignOut: () => _updateUser(null),
-    auth: widget.auth,
+    return StreamBuilder<User>(
+      stream: widget.auth.onAuthStateChanged,
+      builder: (context, snapshot) {
+        if(snapshot.hasData){
+          User user = snapshot.data;
+          if(user == null){
+            return SignInPage(
+              onSignIn: _updateUser,
+              auth: widget.auth,
+            );
+          }
+          return HomePage(
+            onSignOut: () => _updateUser(null),
+            auth: widget.auth, 
+          );
+        } 
+        else{
+          return Scaffold(
+            body: Center(
+              child: CircularProgressIndicator(),
+            ),
+          );
+        }
+      },
     );
+   
+   
     
   }
 }
--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -7,9 +7,11 @@ class User{
 }
 
 abstract class AuthBase{
+  
+  Stream<User> get onAuthStateChanged;
   Future<User> currentUser();
   Future<User> signInAnonymously();
-   Future<void> signOut();
+  Future<void> signOut();
 }
 class Auth implements AuthBase{
   
@@ -20,7 +22,12 @@ class Auth implements AuthBase{
     }
     return User(uid: user.uid);
   }
-   
+  
+  @override
+  Stream<User> get onAuthStateChanged{
+    return _firebaseAuth.onAuthStateChanged.map(_userFromFirebase);
+  }
+
   @override
   Future<User> currentUser() async{
     final user = await _firebaseAuth.currentUser();
